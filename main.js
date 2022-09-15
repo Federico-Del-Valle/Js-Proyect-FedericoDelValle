8f601faa1f972820a35fce87a0ac2e9775a637d0
Federico-Del-Valle <fedexdelvalle@gmail.com>
2022-09-15 20:12:11 -0300

    trabajo entregable 2

--- main.js
+++ main.js
@@ -1,38 +1,67 @@
-const empleados = []
-const salary = []
-const working = []
-const messageName = document.querySelector(".message-name")
-const messageHours = document.querySelector(".message-hours")
-const messageSalary = document.querySelector(".message-salary")
-const messageCargo = document.querySelector(".cargo")
+let empleados = []
+let salary = []
+let hours = []
 const cargos = {
     cargo1: "empleado",
     cargo2: "gerente",
     cargo3: "jefe"
 }
-const buttonForm = document.querySelector("#button-form")
-const nameForm = document.querySelector("#name-form").value
-const workingForm = document.querySelector("#working-form").value
-const salaryForm = document.querySelector("#salary-form").value
+let myData = {
+    nombre: empleados,
+    salario: salary,
+    horas: hours
+}
+let nameForm = document.querySelector("#name-form");
+let salaryForm = document.querySelector("#salary-form");
+let hoursForm = document.querySelector("#hours-form");
+let btnForm = document.querySelector("#btn-form");
+let ulName = document.querySelector("#ul-name");
+let ulSalary = document.querySelector("#ul-salary");
+let ulHours = document.querySelector("#ul-hours");
+let messageCargo = document.querySelector(".cargo")
 
-buttonForm.addEventListener("click", function() {
+
+btnForm.addEventListener("click", function() {
     empleados.push(nameForm.value)
     salary.push(salaryForm.value)
-    working.push(workingForm.value)
-    loadData()
+    hours.push(hoursForm.value)
+    nameForm.value = ""
+    salaryForm.value = ""
+    hoursForm.value = ""
+    showData()
+    let myDataJson = JSON.stringify(myData)
+    console.log(myDataJson)
 })
-function loadData(){
-    let listItems = ""
-    for (let i = 0; i < empleados.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${empleados[i]}'>
-                ${empleados[i]}</a>
-            </li>
-            `
-        }
-    messageName.innerHTML = listItems
+
+
+
+function showData() {
+    let namesSaved = ""
+    let salarySaved = ""
+    let hoursSaved = ""
+    for ( let i = 0; i < empleados.length; i++){
+        localStorage.setItem("nombreEmpleado", empleados[i])
+
+        namesSaved +=` 
+        <li> ${empleados[i]} </li>`
     }
+    for ( let i = 0; i < salary.length; i++){
+        salarySaved +=` 
+        <li> ${"$"} ${salary[i]} </li>`
+    }
+    for ( let i = 0; i < hours.length; i++){
+        hoursSaved +=` 
+        <li> ${hours[i]} ${"hs"} </li>`
+    }
+    ulName.innerHTML = namesSaved
+    ulSalary.innerHTML = salarySaved
+    ulHours.innerHTML = hoursSaved
+}
 
-
-
+if(salary >= 2000 && salary < 3000) {
+    messageCargo.textContent += " " + cargos.cargo1
+} else if (salary < 2000) {
+    messageCargo.textContent += " " + cargos.cargo2
+} else {
+    messageCargo.textContent += " " + cargos.cargo3
+}
